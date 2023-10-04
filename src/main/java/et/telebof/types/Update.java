9022ceb65b532f89083b668cd2773dc26e71cdd2
natanimn <natanimco@gmail.com>
2023-10-04 16:16:05 +0300

    v1.1.2a

--- src/main/java/et/telebof/types/Update.java
+++ src/main/java/et/telebof/types/Update.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  *
  * @see <a href="https://core.telegram.org/bots/api#Update">Telegram documentation</a>"
  * @author Natanim Negash
- * @version 1.1.2
+ * @version 1.1.0
  */
 
 public class Update extends JsonSerializable implements Serializable {
