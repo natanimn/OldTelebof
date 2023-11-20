bb5d993e5209824ec83b02aee1151b4202a45ffe
natanimn <natanimco@gmail.com>
2023-11-20 18:18:03 +0300

    Feat: add method exec

--- src/main/java/et/telebof/requests/SendVoice.java
+++ src/main/java/et/telebof/requests/SendVoice.java
@@ -1,13 +1,11 @@
 package et.telebof.requests;
 
+import et.telebof.Util;
 import et.telebof.types.Message;
 import et.telebof.types.MessageEntity;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class SendVoice extends DefaultParameters<SendVoice, Message> {
     public SendVoice(Object chatId, String voice, RequestSender requestSender) {
@@ -27,6 +25,11 @@ public class SendVoice extends DefaultParameters<SendVoice, Message> {
 
     public SendVoice captionEntities(MessageEntity[] captionEntities) {
         return add("caption_entities", List.of(captionEntities));
+    }
+
+    @Override
+    public Message exec() {
+        return Util.parse(requestSender.makeRequest(this), Message.class);
     }
 
 }
