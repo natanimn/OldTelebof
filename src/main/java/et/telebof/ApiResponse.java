6ee0e860aeda977f32dc781275927fd233c2ba0b
natanimn <natanimco@gmail.com>
2024-01-15 12:06:24 +0300

    Feat: replace Map reference with ResponseParameters reference

--- src/main/java/et/telebof/ApiResponse.java
+++ src/main/java/et/telebof/ApiResponse.java
@@ -2,14 +2,12 @@ package et.telebof;
 
 import et.telebof.types.JsonSerializable;
 
-import java.util.Map;
-
 public class ApiResponse extends JsonSerializable {
     public boolean ok;
     public String description;
     public int error_code;
     public Object result;
-    public Map<String,  Object> parameters;
+    public ResponseParameters parameters;
 
 }
 
